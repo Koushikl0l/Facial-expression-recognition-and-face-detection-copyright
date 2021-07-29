5ef05810f2828fbfb5860fa5641ffd12fbf4d73d
koushik007 <koushikahamed2@gmail.com>
2021-07-30 02:51:40 +0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,16 @@
 mkdir -p ~/.streamlit/
 
+
 echo "\
 [server]\n\
 headless = true\n\
 port = $PORT\n\
 enableCORS = false\n\
 \n\
-"
+" > ~/.streamlit/config.toml
+
+echo "\
+[general]\n\
+email = \"koushikahamed2@gmail.com.br\"\n\
+" > ~/.streamlit/credentials.toml
+© 2021 GitHub, Inc.
